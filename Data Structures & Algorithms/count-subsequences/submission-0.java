da66e6484a7864c94591ccbe9c03ed85008d534d
Gopi Pruthvi <140968046+gopipruthviFE@users.noreply.github.com>
2026-07-16 23:40:52 +0530

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int count = 0;
+        int[] freq = new int[n + 1];
+        for(int i=1;i<m+1;i++){
+            freq[dp[i][n]]++;
+        }
+
+        for(int i=0;i<n+1;i++){
+            if(freq[i] > 1){
+                count += freq[i];
+            }
+        }
+        return count;
+    }
+}
